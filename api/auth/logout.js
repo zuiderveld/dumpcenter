57e27c647a75e019bf72d21a02c3f8413896947a
zuiderveld <zuiderveldroleplay448@gmail.com>
2026-06-10 14:31:44 +0200

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,9 +0,0 @@
-const { getConfig } = require('../../lib/config');
-const { clearSessionCookie } = require('../../lib/session');
-
-module.exports = async (req, res) => {
-  const cfg = getConfig(req);
-  clearSessionCookie(res, cfg);
-  res.writeHead(302, { Location: '/' });
-  res.end();
-};
