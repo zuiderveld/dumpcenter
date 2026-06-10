42628859f66fc5f2ba867ac31639ae8673158746
zuiderveld <zuiderveldroleplay448@gmail.com>
2026-06-10 14:32:07 +0200

    Delete interface directory

--- interface/config.js
+++
@@ -1,6 +0,0 @@
-// Dump Center — alles op Vercel (zelfde origin)
-window.DUMP_CENTER_CONFIG = {
-    apiBase: "",
-    frontendUrl: "https://dumpcenter-m79v.vercel.app",
-    discordClientId: "1133760156052754502",
-};
